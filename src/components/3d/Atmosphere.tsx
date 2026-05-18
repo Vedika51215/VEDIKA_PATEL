e6279d71ad43da78484a10e08aa9dfa5e6dc223d
Vedika51215 <vedika51215@gmail.com>
2026-05-18 10:19:35 +0530

    Fix typescript error

--- src/components/3d/Atmosphere.tsx
+++ src/components/3d/Atmosphere.tsx
@@ -3,6 +3,7 @@
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Points, PointMaterial } from '@react-three/drei';
 import { useState, useRef } from 'react';
+// @ts-expect-error: maath types are missing in some setups
 import * as random from 'maath/random/dist/maath-random.esm';
 
 function Spores(props: any) {
